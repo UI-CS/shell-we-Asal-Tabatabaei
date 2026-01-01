b4122080e2b1cd89f8187408da09d0a0563f176f
Asal Tabatabaei <asal.tabatabaei2022@gmail.com>
2026-01-01 12:11:13 +0330

    history with git add .

--- os.c
+++ os.c
@@ -5,5 +5,26 @@
 #include <sys/wait.h>
 
 
+#define MAX 1024
+
+char last[MAX];
+int main() {
+    char line[MAX];
+
+    while (1) {
+        printf("sh> ");
+        fgets(line, MAX, stdin);
+        line[strcspn(line, "\n")] = 0;
 
 
+        // history
+        if (!strcmp(line, "!!")) {
+            if (!strlen(last)) {
+                printf("No history\n");
+                continue;
+            }
+            strcpy(line, last);
+            printf("%s\n", line);
+        } else
+            strcpy(last, line);}
+            
