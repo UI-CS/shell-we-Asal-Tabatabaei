b380b94c810b945d01f0ccc77c81b6f10c8aeb7f
Asal Tabatabaei <asal.tabatabaei2022@gmail.com>
2026-01-05 12:37:46 +0330

    pipe and Error Handling

--- os.c
+++ os.c
@@ -27,7 +27,7 @@ int main() {
     char *args[MAX];
     int should_run = 1; 
 
-    while (1) {
+    while (should_run) {
         /* reap zombie processes */
         while (waitpid(-1, NULL, WNOHANG) > 0);
 
@@ -87,7 +87,104 @@ int main() {
             continue;
         }
 
+        //background(&)
+        int background = 0;
+        int i = 0;
+        while (args[i] != NULL) i++;
+
+        if (i > 0 && !strcmp(args[i - 1], "&")) {
+            background = 1;
+            args[i - 1] = NULL;
+        }
+
+
+        /* ---------- pipe (|) ---------- */
+        int pipe_index = -1;
+        for (i = 0; args[i] != NULL; i++) {
+            if (strcmp(args[i], "|") == 0) {
+                pipe_index = i;
+                break;
+            }
+        }
+
+        if (pipe_index != -1) {
+            args[pipe_index] = NULL;
+            char **cmd1 = args;
+            char **cmd2 = &args[pipe_index + 1];
+
+            int fd[2];
+            if (pipe(fd) < 0) {
+                perror("pipe failed");
+                continue;
+            }
+
+            pid_t p1 = fork();
+            if (p1 < 0) {
+                perror("Fork failed");
+                continue;
+            }
+
+            if (p1 == 0) {
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[0]);
+                close(fd[1]);
+                if (execvp(cmd1[0], cmd1) == -1) {
+                    printf("Command not found: %s\n", cmd1[0]);
+                    exit(1);
+                }
+            }
+
+            pid_t p2 = fork();
+            if (p2 < 0) {
+                perror("Fork failed");
+                continue;
+            }
+
+            if (p2 == 0) {
+                dup2(fd[0], STDIN_FILENO);
+                close(fd[1]);
+                close(fd[0]);
+                if (execvp(cmd2[0], cmd2) == -1) {
+                    printf("Command not found: %s\n", cmd2[0]);
+                    exit(1);
+                }
+            }
+
+            close(fd[0]);
+            close(fd[1]);
+            wait(NULL);
+            wait(NULL);
+            continue;
+        }
+        
+
+
+         pid_t pid = fork();
+
+        if (pid < 0) {
+            perror("Fork failed");
+            continue;
+        }
+
+        if (pid == 0) {
+            if (execvp(args[0], args) == -1) {
+                printf("Command not found: %s\n", args[0]);
+                exit(1);
+
+            }
+        } else {
+            if (!background)
+                wait(NULL);
+            else
+                printf("[Running in background]\n");
+        }
+    }
+
+    return 0;
+        
+
+
 
 
             
-}}
+}
